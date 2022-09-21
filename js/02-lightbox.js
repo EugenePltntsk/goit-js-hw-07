63cc0e2f4ddcd0fbef6728a6b85220424ec10f36
Eugene Plotnytskyi <eugeneplotnytskyi@gmail.com>
2022-09-21 11:33:47 +0300

    done hw-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,28 @@
 import galleryItems from './gallery-items.js';
 // Change code below this line
 
+const ourUl = document.querySelector('.gallery');
+
+const gallery = galleryItems
+  .map((item) => {
+    return `<li><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`;
+  })
+    .join("");
+  
+ourUl.insertAdjacentHTML("afterbegin", gallery);
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  
+});
 
 
 
-console.log(galleryItems);
+
 
 
 
